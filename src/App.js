9bc6421c8151f0f201e5748309e1ed56f7a6147f
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-11-24 16:27:03 +0000

    Initial commit, Sudoku Grid created and logic attached

---
+++ src/App.js
@@ -0,0 +1,156 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <table>
+        <tr>
+          <th><input type="text" name="c1" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c2" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c3" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c4" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c5" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c6" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c7" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c8" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c9" size="1" maxlength="1"></input></th>
+        </tr>
+        <tr>
+          <th><input type="text" name="c1" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c2" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c3" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c4" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c5" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c6" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c7" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c8" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c9" size="1" maxlength="1"></input></th>
+        </tr>
+        <tr>
+          <th><input type="text" name="c1" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c2" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c3" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c4" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c5" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c6" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c7" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c8" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c9" size="1" maxlength="1"></input></th>
+        </tr>
+        <tr>
+          <th><input type="text" name="c1" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c2" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c3" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c4" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c5" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c6" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c7" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c8" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c9" size="1" maxlength="1"></input></th>
+        </tr>
+        <tr>
+          <th><input type="text" name="c1" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c2" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c3" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c4" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c5" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c6" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c7" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c8" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c9" size="1" maxlength="1"></input></th>
+        </tr>
+        <tr>
+          <th><input type="text" name="c1" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c2" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c3" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c4" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c5" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c6" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c7" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c8" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c9" size="1" maxlength="1"></input></th>
+        </tr>
+        <tr>
+          <th><input type="text" name="c1" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c2" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c3" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c4" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c5" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c6" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c7" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c8" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c9" size="1" maxlength="1"></input></th>
+        </tr>
+        <tr>
+          <th><input type="text" name="c1" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c2" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c3" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c4" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c5" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c6" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c7" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c8" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c9" size="1" maxlength="1"></input></th>
+        </tr>
+        <tr>
+          <th><input type="text" name="c1" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c2" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c3" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c4" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c5" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c6" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c7" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c8" size="1" maxlength="1"></input></th>
+          <th><input type="text" name="c9" size="1" maxlength="1"></input></th>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+function solveSudoku(board) {
+  const n = board.length;
+  recurse(board, n);
+  return board
+}
+
+function recurse(board, n) {
+  for (let row = 0; row < n; row++) {
+    for (let col = 0; col < n; col++) {
+      if (board[row][col] === '.') {
+        const map = {
+          '1': true,
+          '2': true,
+          '3': true,
+          '4': true,
+          '5': true,
+          '6': true,
+          '7': true,
+          '8': true,
+          '9': true,
+        };
+        for(let i=0; i<9; i++) {
+          map[board[i][col]] = false;
+          map[board[row][i]] = false;
+        }
+        const sr = Math.floor(row/3) * 3;
+        const sc = Math.floor(col/3) * 3;
+        for(let k=sr; k<sr+3; k++) {
+          for(let l=sc; l<sc+3; l++) {
+            map[board[k][l]] = false;
+          }
+        }
+        const possibles = Object.entries(map).filter(([k, v]) => v).map(([k, v]) => k);
+        for(let j=0; j<possibles.length; j++) {
+          board[row][col] = possibles[j];
+          if (recurse(board, n)) return true;
+          else board[row][col] = '.';
+        }
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+export default App;
