13995f058f5ef362287d9d9902cf6b0ec20b8e31
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-11-25 20:29:06 +0000

    Particles added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,190 @@
 import './App.css';
+import { useCallback } from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+
+const particleOptions = {
+  
+  fpsLimit: 60,
+  particles: {
+    number: {
+      value: 0,
+      density: {
+        enable: false,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#000"
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000"
+      },
+      polygon: {
+        nb_sides: 5
+      },
+      image: {
+        src: "https://cdn.matteobruni.it/images/particles/github.svg",
+        width: 100,
+        height: 100
+      }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 5,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#000",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+        mode: "repulse",
+        parallax: {
+          enable: false,
+          force: 60,
+          smooth: 10
+        }
+      },
+      onclick: {
+        enable: true,
+        mode: "emitter"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 0.8,
+        speed: 3
+      },
+      repulse: {
+        distance: 50
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true,
+  background: {
+    color: "#fff",
+    image: "",
+    position: "50% 50%",
+    repeat: "no-repeat",
+    size: "cover"
+  },
+  emitters: {
+    direction: "top",
+    position: {
+      x: 50,
+      y: 130
+    },
+    rate: {
+      delay: 0.1
+    },
+    size: {
+      width: 100,
+      height: 0
+    },
+    particles: {
+      color: {
+        value: ["#5bc0eb", "#fde74c", "#9bc53d", "#e55934", "#fa7921"]
+      },
+      lineLinked: {
+        enable: false
+      },
+      opacity: {
+        value: 0.5
+      },
+      size: {
+        value: 400,
+        random: {
+          enable: true,
+          minimumValue: 50
+        }
+      },
+      opacity: {
+        value: 0.5
+      },
+      move: {
+        speed: 10,
+        random: false,
+        outMode: "destroy"
+      }
+    }
+  }
+}
+
+
+
 
 function App() {
+  const particlesInit = useCallback(async engine => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+}, []);
+
+const particlesLoaded = useCallback(async container => {
+    await console.log(container);
+}, []);
   return (
-    <div className="App">
+  <div><h1 align="center">Sudoku Solver</h1>
+  <Particles id="tsparticles" params={particleOptions} init={particlesInit} loaded={particlesLoaded} />
+    <div className="App center">
       <table>
         <tr>
           <th><input type="text" name="c1" size="1" maxLength="1"></input></th>
@@ -106,6 +288,7 @@ function App() {
       </table>
       <button onClick={solve}>Solve Sudoku</button><button onClick={clear}>Clear Board</button>
     </div>
+  </div>
   );
 }
 
@@ -190,5 +373,9 @@ function recurse(board, n) {
   }
   return true;
 }
+
+
+
+
 
 export default App;
